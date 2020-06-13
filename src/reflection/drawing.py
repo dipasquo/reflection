3a101a7290be9c615785aacef4d06bbd07201e38
Dan DiPasquo <dandipasquo@dayzerodiagnostics.com>
2020-06-13 16:44:05 -0700

    Add some drawing, reflect executable

--- src/reflection/drawing.py
+++ src/reflection/drawing.py
@@ -5,13 +5,53 @@ import matplotlib.pyplot as plt
 from typing import List
 
 
-def draw_points(points: List[tuple]) -> None:
+def draw(title: str = None) -> None:
+    """ Show the canvas. """
+    if title:
+        plt.title(title)
+    plt.axis("equal")
+    plt.show()
+
+
+def draw_one_point(point: tuple, style: str = "bo") -> None:
     """ Draw a point.
+    Args:
+        point: (x, y) tuple
+        style (str): drawing style e.g. "b-"
+    """
+    draw_multiple_points([point], style)
+
+
+def draw_multiple_points(points: List[tuple], style: str = "bo") -> None:
+    """ Draw a set of points.
 
     Args:
         points: list of (x, y) tuples
+        style (str): drawing style e.g. "b-"
     """
-    plt.title("Points")
     x_coords, y_coords = zip(*points)
-    plt.scatter(x_coords, y_coords)
-    plt.show()
+    plt.plot(x_coords, y_coords, style)
+
+
+def draw_one_line(endpoints: List[tuple], style: str = "b-"):
+    """ Draw a line connecting a list of exactly two endpoints.
+
+    Args:
+        endpoints (tuple): list of two (x, y) tuples
+        style (str): drawing style e.g. "b-"
+    """
+    assert len(endpoints) == 2, "expected exactly two points to draw a line"
+
+    x_coords, y_coords = zip(*endpoints)
+    plt.plot(x_coords, y_coords, style)
+
+
+def draw_multiple_lines(lines: List[list], style: str = "b-") -> None:
+    """ Draw a set of lines.
+
+    Args:
+        lines (list): list of line endpoints
+        style (str): drawing style e.g. "b-"
+    """
+    for line in lines:
+        draw_one_line(line, style)
--- tests/test_draw.py
+++ tests/test_draw.py
@@ -4,11 +4,24 @@ from reflection import drawing
 from tests.helpers import random_point
 
 
-def test_draw_points():
-    drawing.draw_points(
-        [(-2, -2), (-2, 2), (2, 2), (2, -2)]
-    )
+def test_draw_multiple_points():
+    drawing.draw_multiple_points([(-2, -2), (-2, 2), (2, 2), (2, -2)])
+    drawing.draw()
 
-    drawing.draw_points(
+    drawing.draw_multiple_points(
         [random_point(), random_point(), random_point(), random_point()]
     )
+    drawing.draw()
+
+
+def test_draw_line():
+    drawing.draw_one_line([(-2, -2), (2, 2)])
+    drawing.draw()
+
+    drawing.draw_one_line([(-2, -2), (2, 2)], style="g--")
+    drawing.draw()
+
+
+def test_draw_lines():
+    drawing.draw_multiple_lines([[(0, -2), (0, 2)], [(-2, 0), (2, 0)]])
+    drawing.draw()
